d800dc6afb92ca910d7ad7ccc8eb9412c4127788
Karl Horky <karl.horky@gmail.com>
2020-06-16 10:55:55 +0200

    Configure for Heroku

--- db.js
+++ db.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 
 const postgres = require('postgres');
-const sql = postgres();
+
+const sql =
+  process.env.NODE_ENV === 'production'
+    ? // Heroku needs SSL connections but
+      // has an "unauthorized" certificate
+      // https://devcenter.heroku.com/changelog-items/852
+      postgres({ ssl: { rejectUnauthorized: false } })
+    : postgres();
 
 // const products = [
 //   { id: '1', name: 'T-Shirt' },
