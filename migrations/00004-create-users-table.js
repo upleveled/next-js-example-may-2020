0351cdd51a130885b6cb05576e3f7f33a85922bc
Karl Horky <karl.horky@gmail.com>
2020-07-13 11:05:33 +0200

    Add length to field type

--- migrations/00004-create-users-table.js
+++ migrations/00004-create-users-table.js
@@ -3,8 +3,8 @@ exports.up = async (sql) => {
   sql`
     CREATE TABLE users(
       id SERIAL PRIMARY KEY,
-      username VARCHAR NOT NULL,
-      password_hash VARCHAR NOT NULL
+      username VARCHAR(100) NOT NULL,
+      password_hash VARCHAR(100) NOT NULL
     )
   `;
 };
