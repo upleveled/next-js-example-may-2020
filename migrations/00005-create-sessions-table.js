4c9c962fbf642e13221ac37d5a69208d256fcc4e
Karl Horky <karl.horky@gmail.com>
2020-07-15 15:56:21 +0200

    Add expiry timestamp to sessions table

--- migrations/00005-create-sessions-table.js
+++ migrations/00005-create-sessions-table.js
@@ -3,8 +3,9 @@ exports.up = async (sql) => {
   await sql`
     CREATE TABLE sessions(
       id SERIAL PRIMARY KEY,
-      user_id INT NOT NULL,
-      token VARCHAR(32) NOT NULL
+      expiry_timestamp TIMESTAMP NOT NULL DEFAULT NOW() + INTERVAL '24 hours',
+      token VARCHAR(32) UNIQUE NOT NULL,
+      user_id INT NOT NULL REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
     )
   `;
 };
